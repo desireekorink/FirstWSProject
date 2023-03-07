78e10d450bd5981ccc34d06ca8dd02cb9f507fb3
ITKumait <126791538+ITKumait@users.noreply.github.com>
2023-03-07 13:36:32 +0100

    I have change the Sout

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -2,6 +2,7 @@ package MainPackage;
 
 public class MainClass {
     public static void main(String[] args) {
+        System.out.println("this is a change");
 
     }
 }
---
+++ src/MainPackage/SecondClass.java
@@ -0,0 +1,8 @@
+package MainPackage;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("this is second change");
+
+    }
+}
